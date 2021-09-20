dcd6aad7073394cf1c0f1d2325e4bad898f51092
jani leskela <n0leja00@students.oamk.fi>
2021-09-20 19:15:55 +0300

    creating repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState} from 'react';
 
-function App() {
+function App () {
+  const [weight,setWeight] = useState(0);
+  const [bottles,setBottles] = useState(1);
+  const [time,setTime] = useState(1)
+  const [gender,setGender] = useState('male')
+  const [result,setResult] = useState(0)
+
+  function calculate(e){
+    e.preventDefault();
+    const litres = bottles * 0.33;
+    const grams = litres * 8 * 4.5;
+    const burning = weight / 10;
+    const gramsl = grams - (burning * time)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container"> 
+      <h3>Calculating blood alcohol level</h3>
+
+    <form onSubmit={calculate}>
+    
+      <div>
+        <label>Weight</label>
+        </div>
+      <input type='number' step='1'
+      value={weight} 
+      onChange={e => setWeight(e.target.value)} />
+      <div>
+        <label>Bottles</label>
+      </div>
+      <select 
+      value={bottles} 
+      onChange={e => setBottles(e.target.value)}>
+        <option value='1'> 1</option>
+        <option value='2'> 2</option>
+      </select>
+
+      <div>
+        <label>Time</label>
+      </div>
+      <select
+      value={time} 
+      onChange={e => setTime(e.target.value)}
+      >
+        <option value='1'> 1</option>
+        <option value='2'> 2</option>
+      </select>
+      <div>
+        <label>Gender</label>
+        <input type="radio" name="gender" value="male" defaultChecked/><label> Male</label>
+        <input type="radio" name="gender" value="female"/> <label>Female</label>
+         </div>
+
+      <div>
+        <output>{result}</output>
+      </div>
+        <button> Calculate</button>
+    </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
